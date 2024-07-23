afab762757e91394db8b95b2a62e14ef27500120
break-stuff <burton@breakstuff.io>
2024-07-23 14:53:20 -0400

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import CodeBubble from './components/code-bubble/code-bubble.js';
 import { CodeBubbleConfig, updateConfig } from './configs/sandbox-configs.js';
 import { syncSandboxes } from './utilities/sandbox-sync.js';
 
+export { CodeBubbleConfig } from './configs/sandbox-configs.js';
+
 export function codeBubble(userConfig?: CodeBubbleConfig) {
   updateConfig(userConfig);
   customElements.define('code-bubble', CodeBubble);
