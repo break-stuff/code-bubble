14ec681c724b0194bfd03bee0188839afbbf8927
Burton Smith <burton@breakstuff.io>
2024-07-25 11:14:01 -0400

    fix config order

--- src/configs/component-config.ts
+++ src/configs/component-config.ts
@@ -1,31 +1,33 @@
 export type ComponentConfig = {
   /** Opens the "show code" section by default */
   openShowCode?: boolean;
-  /** Hides the "show code" button */
-  hideShowCodeButton?: boolean;
-  /** Text displayed in the "show code" button  */
-  showCodeButtonLabel?: string;
+  /** Indicates which example to show by default */
+  defaultExample?: 'html' | 'react';
   /** Hides the "copy code" button */
   hideCopyCodeButton?: boolean;
+  /** Hides the "RTL" button */
+  hideRtlButton?: boolean;
+  /** Hides the "sandbox" button */
+  hideSandboxButton?: boolean;
+  /** Hides the "show code" button */
+  hideShowCodeButton?: boolean;
+  /** Hides the preview window where the code is rendered */
+  hidePreview?: boolean;
+  /** Text displayed in the "show code" button  */
+  showCodeButtonLabel?: string;
   /** Text displayed in the "copy code" button  */
   copyCodeButtonLabel?: string;
   /** Text displayed in the "copy code" button when text has been copied  */
   copyCodeButtonCopiedLabel?: string;
-  /** Indicates which example to show by default */
-  defaultExample?: 'html' | 'react';
-  /** Hides the "RTL" button */
-  hideRtlButton?: boolean;
   /** Text displayed in the "RTL" button  */
   rtlButtonLabel?: string;
-  /** Hides the "sandbox" button */
-  hideSandboxButton?: boolean;
   /** Text displayed in the "sandbox" button  */
   sandboxButtonLabel?: string;
   /** Text displayed in the "HTML" button  */
   htmlButtonLabel?: string;
   /** Text displayed in the "React" button  */
   reactButtonLabel?: string;
-}
+};
 
 export const componentConfig: ComponentConfig = {
   openShowCode: false,
@@ -40,5 +42,5 @@ export const componentConfig: ComponentConfig = {
   hideSandboxButton: false,
   sandboxButtonLabel: 'Sandbox',
   htmlButtonLabel: 'HTML',
-  reactButtonLabel: 'React'
+  reactButtonLabel: 'React',
 };
