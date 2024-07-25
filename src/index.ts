756fb094c0567a7fd31001d4b9de5b75f8a15808
break-stuff <burton@breakstuff.io>
2024-07-25 15:04:53 -0400

    add `setLanguage` function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import CodeBubble from './components/code-bubble/code-bubble.js';
+import CodeBubble, { CodeExamples } from './components/code-bubble/code-bubble.js';
 import { CodeBubbleConfig, updateConfig } from './configs/sandbox-configs.js';
 import { syncSandboxes } from './utilities/sandbox-sync.js';
 
 export type { CodeBubbleConfig } from './configs/sandbox-configs.js';
+
+export function setLanguage(lang: string) {
+  document.querySelectorAll<CodeBubble>('[code-bubble]').forEach(y => {
+    y.framework = lang as CodeExamples;
+  });
+  localStorage.setItem('code_block_example', lang);
+}
 
 export function codeBubble(userConfig?: CodeBubbleConfig) {
   updateConfig(userConfig);
